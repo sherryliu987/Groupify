5aa469ce77286c8ac8b35d022751280305b55fce
Sherry <sherryliu987@gmail.com>
2021-01-24 13:18:52 -0500

    groups page, fix file request

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 
-UPLOAD_FOLDER = "/uploads"
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "secret_key"
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -2,39 +2,52 @@ import os.path
 from os import path, abort
 from flask import send_from_directory, flash, redirect, url_for, render_template, request
 from werkzeug.utils import secure_filename
+import pandas as pd
+from itertools import permutations
 
 from application import app
 
-ALLOWED_EXTENSIONS = {'csv', 'xlsx'}
+ALLOWED_EXTENSIONS = {'xls'}
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/", methods=['GET', 'POST'])
-@app.route("/main",  methods=['GET', 'POST'])
-def home():
+@app.route("/main", methods=['GET', 'POST'])
+def home(message=""):
     if request.method == 'POST':
+        print(request.form)
+        num_groups = request.form.get('num_groups')
         if 'people' not in request.files:
-            return redirect(request.url)
+            return render_template("main.html", message="Please choose a valid file.")
         file = request.files['people']
         if file.filename == '':
-            return redirect(request.url)
+            return render_template("main.html", message="Please choose a valid file.")
+        if num_groups == '':
+            return render_template("main.html", message="Please input your desired number of groups.")
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+            filename = file.filename
             file.save(request.files['people'].filename)
-            return redirect(url_for('groups', filename=filename))
-    return render_template("main.html")
+            return redirect(url_for('groups', filename=filename, num_groups=num_groups))
+    return render_template("main.html", message=message)
 
-@app.route("/groups/<filename>")
-def groups(filename="/"):
-    f = open(filename)
-    groups=[
+
+@app.route("/groups/<filename>/<num_groups>")
+def groups(filename="/", num_groups=1):
+    f=send_from_directory("../", filename)
+
+    df = pd.read_excel(filename)
+    print(df)
+    print(num_groups)
+    final_groups = [
         {"id": "1", "people": {"Bob", "Joe"}},
         {"id": "2", "people": {"Bob", "Joe"}},
         {"id": "3", "people": {"Bob", "Joe"}}
     ]
-    return render_template("groups.html", groups = groups)
+    return render_template("groups.html", groups=final_groups)
+
 
 @app.route("/about")
 def about():
